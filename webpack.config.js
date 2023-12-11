1759f09631ec4b1b2d867357060f278726a2a270
Dongsheng Cai <d@tux.im>
2023-12-11 23:02:39 +1100

    chore: webpack executable

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const webpack = require('webpack');
+const fs = require('node:fs');
+
+class MakeExecutable {
+  constructor(options = { fileList: [] }) {
+    this.options = { ...options };
+  }
+  apply(compiler) {
+    compiler.hooks.done.tapPromise(
+      'MakeExecutable',
+      () =>
+        new Promise((resolve) => {
+          this.options.fileList.forEach((filepath) => {
+            if (fs.existsSync(filepath)) {
+              fs.chmodSync(filepath, '755');
+            }
+          });
+          resolve();
+        }),
+    );
+  }
+}
 
 module.exports = {
   devtool: false,
@@ -9,6 +30,9 @@ module.exports = {
   externals: [nodeExternals()],
   plugins: [
     new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+    new MakeExecutable({
+      fileList: [`${__dirname}/dist/app.js`],
+    }),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
