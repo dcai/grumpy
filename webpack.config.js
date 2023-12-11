d1e9d427810a121cf61724e538341acba163cbc5
Dongsheng Cai <d@tux.im>
2023-12-11 21:57:18 +1100

    feat: use bundler

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: false,
+  entry: './src/index',
+  target: 'node',
+  externals: [nodeExternals()],
+  plugins: [
+    new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+};
